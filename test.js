a96a11f0988d789ca455e44744f36ef1d5b55ed7
Federico Brigante <bfred-it@users.noreply.github.com>
2019-04-12 10:36:47 +0700

    more tests

--- test.js
+++ test.js
@@ -1,17 +1,54 @@
 const test = require('tape');
 const insertText = require('.');
 
-const getField = (repeat = 50) => {
+const getField = (value = '') => {
 	const field = document.createElement('textarea');
-	field.textContent = 'Lorem ipsum dolor sit amet'.repeat(repeat);
+	field.value = value;
 	document.body.append(field);
 	return field;
 };
 
 test('insert text in empty field', t => {
-	t.plan(2);
-	const textarea = getField(0);
+	t.plan(4);
+	const textarea = getField();
 	t.equal(textarea.value, '');
-	insertText(textarea, 'hello world');
-	t.equal(textarea.value, 'hello world');
+	insertText(textarea, 'a');
+	t.equal(textarea.value, 'a');
+	t.equal(textarea.selectionStart, 1);
+	t.equal(textarea.selectionEnd, 1);
 });
+
+test('append text to unselected field', t => {
+	t.plan(4);
+	const textarea = getField('W');
+	t.equal(textarea.value, 'W');
+	insertText(textarea, 'O');
+	t.equal(textarea.value, 'WO');
+	t.equal(textarea.selectionStart, 2);
+	t.equal(textarea.selectionEnd, 2);
+});
+
+test('insert text in the middle', t => {
+	t.plan(4);
+	const textarea = getField('WO');
+	t.equal(textarea.value, 'WO');
+	textarea.selectionStart = 1;
+	textarea.selectionEnd = 1;
+	insertText(textarea, 'A');
+	t.equal(textarea.value, 'WAO');
+	t.equal(textarea.selectionStart, 2);
+	t.equal(textarea.selectionEnd, 2);
+});
+
+test('replace selected text', t => {
+	t.plan(4);
+	const textarea = getField('WO');
+	t.equal(textarea.value, 'WO');
+	textarea.selectionStart = 0;
+	textarea.selectionEnd = 1;
+	insertText(textarea, 'A');
+	t.equal(textarea.value, 'AO');
+	t.equal(textarea.selectionStart, 1);
+	t.equal(textarea.selectionEnd, 1);
+});
+
