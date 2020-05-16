ac4f957e6efc20d28ae1fa4cda1d09ccc8ac6cd8
Federico Brigante <opensource@bfred.it>
2020-05-16 22:49:03 +0200

    Simplify tests

--- test.js
+++ test.js
@@ -1,17 +1,35 @@
 import test from 'tape';
 import * as textFieldEdit from '.';
 
-const getField = (value = '', start = undefined, end = undefined, type = 'textarea') => {
+function getField(state = '|', type = 'textarea') {
 	const field = document.createElement(type);
-	field.value = value;
+	const cursor = state.indexOf('|');
+	const selectionStart = state.indexOf('{');
+	const selectionEnd = state.indexOf('}') - 1;
+	field.value = state.replace(/[{|}]/g, '');
+	field.selectionStart = cursor >= 0 ? cursor : selectionStart;
+	field.selectionEnd = cursor >= 0 ? cursor : selectionEnd;
 	document.body.append(field);
-	if (end !== undefined) {
-		field.selectionStart = start;
-		field.selectionEnd = end;
+	return field;
+}
+
+function getState({value, selectionStart, selectionEnd}) {
+	if (selectionStart === selectionEnd) {
+		return value.slice(0, selectionStart) + '|' + value.slice(selectionStart);
 	}
 
-	return field;
-};
+	return value.slice(0, selectionStart) + '{' + value.slice(selectionStart, selectionEnd) + '}' + value.slice(selectionEnd);
+}
+
+test('test harness test', t => {
+	t.equal(getState(getField()), '|');
+	t.equal(getState(getField('|')), '|');
+	t.equal(getState(getField('A|')), 'A|');
+	t.equal(getState(getField('|A')), '|A');
+	t.equal(getState(getField('{A}')), '{A}');
+	t.equal(getState(getField('A{B}C')), 'A{B}C');
+	t.end();
+});
 
 test('insert() preserves focused item, if focusable', t => {
 	t.equal(document.activeElement, document.body);
@@ -23,51 +41,36 @@ test('insert() preserves focused item, if focusable', t => {
 
 test('insert() inserts text in empty field', t => {
 	const field = getField();
-	t.equal(field.value, '');
 	textFieldEdit.insert(field, 'a');
-	t.equal(field.value, 'a');
-	t.equal(field.selectionStart, 1);
-	t.equal(field.selectionEnd, 1);
+	t.equal(getState(field), 'a|');
 	t.end();
 });
 
 test('insert() inserts text in empty input element', t => {
-	const field = getField('', undefined, undefined, 'input');
-	t.equal(field.value, '');
+	const field = getField('|', 'input');
 	textFieldEdit.insert(field, 'a');
-	t.equal(field.value, 'a');
-	t.equal(field.selectionStart, 1);
-	t.equal(field.selectionEnd, 1);
+	t.equal(getState(field), 'a|');
 	t.end();
 });
 
 test('insert() appends text to unselected field', t => {
-	const field = getField('W');
-	t.equal(field.value, 'W');
+	const field = getField('W|');
 	textFieldEdit.insert(field, 'O');
-	t.equal(field.value, 'WO');
-	t.equal(field.selectionStart, 2);
-	t.equal(field.selectionEnd, 2);
+	t.equal(getState(field), 'WO|');
 	t.end();
 });
 
 test('insert() inserts text in the middle', t => {
-	const field = getField('WO', 1, 1);
-	t.equal(field.value, 'WO');
+	const field = getField('W|O');
 	textFieldEdit.insert(field, 'A');
-	t.equal(field.value, 'WAO');
-	t.equal(field.selectionStart, 2);
-	t.equal(field.selectionEnd, 2);
+	t.equal(getState(field), 'WA|O');
 	t.end();
 });
 
 test('insert() replaces selected text', t => {
-	const field = getField('WO', 0, 1);
-	t.equal(field.value, 'WO');
+	const field = getField('{W}O');
 	textFieldEdit.insert(field, 'A');
-	t.equal(field.value, 'AO');
-	t.equal(field.selectionStart, 1);
-	t.equal(field.selectionEnd, 1);
+	t.equal(getState(field), 'A|O');
 	t.end();
 });
 
@@ -82,51 +85,41 @@ test('insert() fires input event', t => {
 });
 
 test('set() replaces the whole content', t => {
-	const field = getField('WO', 0, 1);
-	t.equal(field.value, 'WO');
+	const field = getField('{W}O');
 	textFieldEdit.set(field, 'ABC');
-	t.equal(field.value, 'ABC');
-	t.equal(field.selectionStart, 3);
-	t.equal(field.selectionEnd, 3);
+	t.equal(getState(field), 'ABC|');
 	t.end();
 });
 
 test('getSelection()', t => {
-	const field = getField('WOA', 1, 2);
-	t.equal(field.value, 'WOA');
+	const field = getField('W{O}A');
 	t.equal(textFieldEdit.getSelection(field), 'O');
 	t.end();
 });
 
 test('getSelection() without selection', t => {
-	const field = getField('WOA', 3, 3);
+	const field = getField('WOA|');
 	t.equal(textFieldEdit.getSelection(field), '');
 	t.end();
 });
 
 test('wrapSelection() wraps selected text', t => {
-	const field = getField('WOA', 1, 2);
+	const field = getField('W{O}A');
 	textFieldEdit.wrapSelection(field, '*');
-	t.equal(field.value, 'W*O*A');
-	t.equal(field.selectionStart, 2);
-	t.equal(field.selectionEnd, 3);
+	t.equal(getState(field), 'W*{O}*A');
 	t.end();
 });
 
 test('wrapSelection() wraps selected text with different characters', t => {
-	const field = getField('WOA', 1, 2);
+	const field = getField('W{O}A');
 	textFieldEdit.wrapSelection(field, '[', ']');
-	t.equal(field.value, 'W[O]A');
-	t.equal(field.selectionStart, 2);
-	t.equal(field.selectionEnd, 3);
+	t.equal(getState(field), 'W[{O}]A');
 	t.end();
 });
 
 test('wrapSelection() adds wrapping characters even without selection', t => {
-	const field = getField('OA', 1, 1);
+	const field = getField('O|A');
 	textFieldEdit.wrapSelection(field, '[', ']');
-	t.equal(field.value, 'O[]A');
-	t.equal(field.selectionStart, 2);
-	t.equal(field.selectionEnd, 2);
+	t.equal(getState(field), 'O[|]A');
 	t.end();
 });
