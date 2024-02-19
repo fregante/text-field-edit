fbeee09f63883c897b4a74b81f3ee6ff7efe1e3d
Federico Brigante <me@fregante.com>
2024-02-19 16:18:55 +0700

    Actually plug it in

--- index.test.ts
+++ index.test.ts
@@ -13,9 +13,9 @@ function isNativeField(field: HTMLElement): field is NativeField {
 	return field instanceof HTMLTextAreaElement || field instanceof HTMLInputElement;
 }
 
-function getField(state: string, type: 'contenteditable'): HTMLElement;
-function getField(state?: string, type?: 'textarea' | 'input'): NativeField;
-function getField(state = '|', type = 'textarea') {
+function getField(type: 'contenteditable', state?: string): HTMLElement;
+function getField(type: 'textarea' | 'input', state?: string): NativeField;
+function getField(type: string, state = '|') {
 	const field = document.createElement(type);
 	if (type === 'contenteditable') {
 		field.setAttribute('contenteditable', 'true');
@@ -62,69 +62,69 @@ function getState(field: HTMLElement) {
 	);
 }
 
-for (const type of ['textarea', 'input']) {
+for (const type of ['textarea', 'input'] as const) {
 	test(`${type}: test harness test`, t => {
-		t.equal(getState(getField()), '|');
-		t.equal(getState(getField('|')), '|');
-		t.equal(getState(getField('A|')), 'A|');
-		t.equal(getState(getField('|A')), '|A');
-		t.equal(getState(getField('{A}')), '{A}');
-		t.equal(getState(getField('A{B}C')), 'A{B}C');
+		t.equal(getState(getField(type)), '|');
+		t.equal(getState(getField(type, '|')), '|');
+		t.equal(getState(getField(type, 'A|')), 'A|');
+		t.equal(getState(getField(type, '|A')), '|A');
+		t.equal(getState(getField(type, '{A}')), '{A}');
+		t.equal(getState(getField(type, 'A{B}C')), 'A{B}C');
 		t.end();
 	});
 
 	test(`${type}: insert() preserves focused item, if focusable`, t => {
 		t.equal(document.activeElement, document.body);
-		const field = getField();
+		const field = getField(type);
 		insertTextIntoField(field, 'A');
 		t.equal(document.activeElement, document.body);
 		t.end();
 	});
 
 	test(`${type}: insert() inserts text in empty field`, t => {
-		const field = getField();
+		const field = getField(type);
 		insertTextIntoField(field, 'a');
 		t.equal(getState(field), 'a|');
 		t.end();
 	});
 
 	test(`${type}: insert() inserts text in empty input element`, t => {
-		const field = getField('|');
+		const field = getField(type, '|');
 		insertTextIntoField(field, 'a');
 		t.equal(getState(field), 'a|');
 		t.end();
 	});
 
 	test(`${type}: insert() appends text to unselected field`, t => {
-		const field = getField('W|');
+		const field = getField(type, 'W|');
 		insertTextIntoField(field, 'O');
 		t.equal(getState(field), 'WO|');
 		t.end();
 	});
 
 	test(`${type}: insert() inserts text in the middle`, t => {
-		const field = getField('W|O');
+		const field = getField(type, 'W|O');
 		insertTextIntoField(field, 'A');
 		t.equal(getState(field), 'WA|O');
 		t.end();
 	});
 
 	test(`${type}: insert() replaces selected text`, t => {
-		const field = getField('{W}O');
+		const field = getField(type, '{W}O');
 		insertTextIntoField(field, 'A');
 		t.equal(getState(field), 'A|O');
 		t.end();
 	});
 
 	test(`${type}: insert() replaces selected text even if string is ""`, t => {
-		const field = getField('{W}O');
+		const field = getField(type, '{W}O');
 		insertTextIntoField(field, '');
 		t.equal(getState(field), '|O');
 		t.end();
 	});
 
 	test(`${type}: insert() fires input event`, t => {
-		const field = getField();
+		const field = getField(type);
 		field.addEventListener('input', event => {
 			t.equal(event.type, 'input');
 			// TODO: t.equal(event.inputType, 'insert');
@@ -134,47 +134,47 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: set() replaces the whole content`, t => {
-		const field = getField('{W}O');
+		const field = getField(type, '{W}O');
 		setFieldText(field, 'ABC');
 		t.equal(getState(field), 'ABC|');
 		t.end();
 	});
 
 	test(`${type}: getSelection()`, t => {
-		const field = getField('W{O}A');
+		const field = getField(type, 'W{O}A');
 		t.equal(getFieldSelection(field), 'O');
 		t.end();
 	});
 
 	test(`${type}: getSelection() without selection`, t => {
-		const field = getField('WOA|');
+		const field = getField(type, 'WOA|');
 		t.equal(getFieldSelection(field), '');
 		t.end();
 	});
 
 	test(`${type}: wrapSelection() wraps selected text`, t => {
-		const field = getField('W{O}A');
+		const field = getField(type, 'W{O}A');
 		wrapFieldSelection(field, '*');
 		t.equal(getState(field), 'W*{O}*A');
 		t.end();
 	});
 
 	test(`${type}: wrapSelection() wraps selected text with different characters`, t => {
-		const field = getField('W{O}A');
+		const field = getField(type, 'W{O}A');
 		wrapFieldSelection(field, '[', ']');
 		t.equal(getState(field), 'W[{O}]A');
 		t.end();
 	});
 
 	test(`${type}: wrapSelection() adds wrapping characters even without selection`, t => {
-		const field = getField('O|A');
+		const field = getField(type, 'O|A');
 		wrapFieldSelection(field, '[', ']');
 		t.equal(getState(field), 'O[|]A');
 		t.end();
 	});
 
 	test(`${type}: replace() supports strings`, t => {
-		const field = getField('ABACUS');
+		const field = getField(type, 'ABACUS');
 		replaceFieldText(field, 'A', 'THE ');
 		t.equal(getState(field), '{THE }BACUS');
 		replaceFieldText(field, 'BA', 'AR');
@@ -183,7 +183,7 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: replace() supports strings with cursor placed after replaced text`, t => {
-		const field = getField('ABACUS');
+		const field = getField(type, 'ABACUS');
 		replaceFieldText(field, 'A', 'THE ', 'after-replacement');
 		t.equal(getState(field), 'THE |BACUS');
 		replaceFieldText(field, 'BA', 'AR', 'after-replacement');
@@ -192,7 +192,7 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: replace() supports strings with a replacer function`, t => {
-		const field = getField('ABACUS');
+		const field = getField(type, 'ABACUS');
 		replaceFieldText(field, 'A', (match, index, string) => {
 			t.equal(match, 'A');
 			t.equal(index, 0);
@@ -204,7 +204,7 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: replace() supports regex`, t => {
-		const field = getField('ABACUS');
+		const field = getField(type, 'ABACUS');
 		replaceFieldText(field, /a/i, 'U');
 		t.equal(getState(field), '{U}BACUS');
 		replaceFieldText(field, /[ab]{2}/i, 'NI');
@@ -213,7 +213,7 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: replace() supports regex with a replacer function`, t => {
-		const field = getField('ABACUS');
+		const field = getField(type, 'ABACUS');
 		replaceFieldText(field, /a/i, (match, index, string) => {
 			t.equal(match, 'A');
 			t.equal(index, 0);
@@ -225,7 +225,7 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: replace() supports regex with groups with a replacer function`, t => {
-		const field = getField('ABACUS');
+		const field = getField(type, 'ABACUS');
 		replaceFieldText(field, /ba(c)us/i, (match, group1, index) => {
 			t.equal(match, 'BACUS');
 			t.equal(index, 1);
@@ -237,21 +237,21 @@ for (const type of ['textarea', 'input']) {
 	});
 
 	test(`${type}: replace() supports regex with groups with replacement patterns (not supported)`, t => {
-		const field = getField('ABA');
+		const field = getField(type, 'ABA');
 		replaceFieldText(field, /(b)/i, '($1)');
 		t.equal(getState(field), 'A{($1)}A'); // TODO: This should be A(B)A
 		t.end();
 	});
 
 	test(`${type}: replace() supports regex with global flag`, t => {
-		const field = getField('1a23cd456');
+		const field = getField(type, '1a23cd456');
 		replaceFieldText(field, /\d+/g, '**');
 		t.equal(getState(field), '**a**cd{**}');
 		t.end();
 	});
 
 	test(`${type}: replace() supports regex with global flag with a replacer function`, t => {
-		const field = getField('Abacus');
+		const field = getField(type, 'Abacus');
 		let i = 0;
 		replaceFieldText(field, /a/gi, (match, index, string) => {
 			if (i === 0) {
